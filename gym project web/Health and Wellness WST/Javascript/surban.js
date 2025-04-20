084c9cc8a229121aace3953711695f1952a13bd3
Mj Ancajas <ancajasmj@gmail.com>
2025-04-20 19:15:49 +0800

    Add files via upload

---
+++ gym project web/Health and Wellness WST/Javascript/surban.js
@@ -0,0 +1,17 @@
+function confirmNavigation() {
+
+    var confirmation = confirm("Are you sure you want to proceed to another page?");
+
+    if (confirmation) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+function confirmlogin()
+{
+   alert("Success! now redirecting to home page!");
+}
